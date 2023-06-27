ca2851164e1462ad5d62a7a557ade72ad4cbf0eb
Nihal Avulan <nihalavulan1@gmail.com>
2023-06-28 00:42:42 +0530

    Hero completed

--- src/Components/Home/Hero/Hero.jsx
+++ src/Components/Home/Hero/Hero.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 import { StyledComponent } from "./Hero.styled";
 import Navbar from "./Navbar/Navbar";
+import { Container } from "../../Common/Containers/StyledContainer";
+import { Icon } from "@iconify/react";
 
 const Hero = () => {
   return (
     <StyledComponent.StyledHeroWrapper>
       <Navbar />
+      <Container>
+        <StyledComponent.HeroContentWrapper>
+          <StyledComponent.HeroH1Text>
+            {" "}
+            <span>Selling online</span> just got <span>easier</span> than ever
+            before.
+          </StyledComponent.HeroH1Text>
+          <StyledComponent.HeroParagraph>
+            Experience the future of Instagram shop management. We empowers
+            sellers to effortlessly handle customers, gain insights, and drive
+            remarkable sales growth. Expand your online presence with
+            confidence.
+          </StyledComponent.HeroParagraph>
+          <StyledComponent.PrimaryButton>
+            <p>Explore Demo</p> <Icon icon="mingcute:right-fill" />
+          </StyledComponent.PrimaryButton>
+        </StyledComponent.HeroContentWrapper>
+      </Container>
     </StyledComponent.StyledHeroWrapper>
   );
 };
--- src/Components/Home/Hero/Hero.styled.js
+++ src/Components/Home/Hero/Hero.styled.js
@@ -6,4 +6,76 @@ export const StyledComponent = {
     height: 100vh;
     background-color: ${({ theme }) => theme.color.primary};
   `,
+  HeroContentWrapper: styled.div`
+    text-align: center;
+    height: 80vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  `,
+  HeroH1Text: styled.h1`
+    font-size: 6.25rem;
+    font-weight: 300;
+    line-height: 116px;
+    color: ${({ theme }) => theme.color.white};
+    span {
+      font-weight: 600;
+    }
+
+    @media (max-width: ${({ theme }) => theme.device.large}) {
+      font-size: 3.25rem;
+      line-height: 86px;
+    }
+    @media (max-width: ${({ theme }) => theme.device.small}) {
+      font-size: 2.25rem;
+      line-height: 56px;
+    }
+  `,
+  HeroParagraph: styled.p`
+    font-size: 18px;
+    color: #a4a4a4;
+    max-width: 70%;
+    font-weight: 200;
+    margin-top: 1rem;
+    line-height: 33px;
+
+    @media (max-width: ${({ theme }) => theme.device.large}) {
+      font-size: 14px;
+      line-height: 33px;
+      max-width: 90%;
+    }
+    @media (max-width: ${({ theme }) => theme.device.small}) {
+      font-size: 10px;
+      line-height: 23px;
+    }
+  `,
+  PrimaryButton: styled.div`
+    border-radius: 25px;
+    background-color: ${({ theme }) => theme.color.secondary};
+    padding: 24px 72px;
+    outline: none;
+    border: none;
+    margin-top: 3rem;
+
+    color: #03045e;
+    text-align: center;
+    font-size: 18px;
+    font-weight: 600;
+    cursor: pointer;
+
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+
+    :hover {
+      background-color: #a0a1fa;
+    }
+
+    @media (max-width: ${({ theme }) => theme.device.large}) {
+      padding: 14px 62px;
+      border-radius: 15px;
+      font-size: 12px;
+    }
+  `,
 };
